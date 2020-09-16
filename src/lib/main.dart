ecfe4df7c691d972dc779550ed4874cb190b78d0
Yoann <yoann.rey@epitech.eu>
2020-09-16 23:25:00 +0200

    add the best button never seen before

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:spotify/spotify.dart' as prefix;
 
 void main() async {
-  final credentials = prefix.SpotifyApiCredentials('d1d31f582c514ff483a45461f4a51003', '7005271fd0b2451e96ee6ea5bbae9d61');
+ /* final credentials = prefix.SpotifyApiCredentials('d1d31f582c514ff483a45461f4a51003', '7005271fd0b2451e96ee6ea5bbae9d61');
   final spotify = prefix.SpotifyApi(credentials);
-  var user = await spotify.me;
-  user);
-  //  runApp(BlindSpot());
+  var user = spotify.me
+  .get();
+
+  print(user.asStream().first); */
+   runApp(BlindSpot());
 }
 
 
@@ -16,6 +18,7 @@ class BlindSpot extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
+      theme: ThemeData(fontFamily: 'Kufam'),
       home: Login()
     );
   }
@@ -36,22 +39,32 @@ class _LoginState extends State<Login> {
           children: <Widget>[
             const SizedBox(height: 50),
             Image.asset('assets/autocad-dxf-hello-world-png-clip-art.png'),
-            const SizedBox(height: 600),
-            ButtonTheme(
-              minWidth: 300,
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                color: Colors.green,
+            const SizedBox(height: 400),
+            /*RaisedButton.icon(
+              
+              label: Text('Connect with'),
+              shape: 
+              icon: Image.asset('assets/Spotify_Logo_RGB_Black.png', width: 90),
 
-                onPressed: () {},
-                child: const Text(
-                    'Connect with ',
-                    style: TextStyle(fontSize: 20)
-                ),
+              onPressed: () {},
+              
+            )*/
+            RaisedButton(
+              onPressed: () {},
+              color: Color(0xff1DB954),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14.0)
               ),
-            )
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text('Connect with ',
+                    style: TextStyle(fontFamily: 'Boire')),
+                  Image.asset('assets/Spotify_Logo_RGB_Black.png', width: 80)
+                ],
+              ),
+              /*color: Colors.purple,*/
+            ),
           ],
         )
       )
