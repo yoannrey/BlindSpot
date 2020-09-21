8ce1092c7868002167e55625edc57a598a1866b8
Yoann <yoann.rey@epitech.eu>
2020-09-21 16:07:48 +0200

    adding the attempt to connect api

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:spotify/spotify.dart' as prefix;
-import 'package:src/SizeConfig.dart';
+//import 'package:src/SizeConfig.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:uni_links/uni_links.dart';
+
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final credentials = prefix.SpotifyApiCredentials('d1d31f582c514ff483a45461f4a51003', '7005271fd0b2451e96ee6ea5bbae9d61');
-  final spotify = prefix.SpotifyApi(credentials);
-  var user = await spotify.me.get();
-  print(user.id);
- //  runApp(BlindSpot());
+  final grant = prefix.SpotifyApi.authorizationCodeGrant(credentials);
+  final redirectUri = 'https://www.example.com/auth';
+  final scopes = ['user-read-email', 'user-library-read'];
+  final authUri = grant.getAuthorizationUrl(
+      Uri.parse(redirectUri),
+      scopes: scopes// scopes are optional
+  );
+  if (await canLaunch(authUri.toString())) {
+    await launch(authUri.toString());
+  }
+
+  if (await canLaunch(authUri.toString())) {
+    await launch(authUri.toString());
+  }
+  var responseUri = "";
+  // ignore: cancel_subscriptions
+  final linksStream = getLinksStream().listen((String link)  {
+    if (link.startsWith(redirectUri)) {
+      responseUri = link;
+    }
+  });
+
+  final spotify = prefix.SpotifyApi.fromAuthCodeGrant(grant, responseUri);
+  // runApp(BlindSpot());
 }
-
-
 
 class BlindSpot extends StatelessWidget {
   @override
